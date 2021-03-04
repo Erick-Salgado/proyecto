7cae08fbdfc71679ba26b96caa1160faabeff4af
Erick-Salgado <erick060998@gmail.com>
2021-03-04 05:26:21 -0600

    primer commit de mi sitio web

---
+++ js/menu.js
@@ -0,0 +1,7 @@
+let btnMenu = document.getElementById('btnmenu');
+let Menu = document.getElementById('menu');
+
+btnMenu.addEventListener('click',function(){
+    'use strict';
+    Menu.classList.toggle('mostrar');
+})
